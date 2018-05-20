cb4c5592fc9192a049403aa57b8acdfc52579cd3
pandas-seven <34061140+pandas-seven@users.noreply.github.com>
2018-05-20 05:58:14 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,44 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-from utils.helper import *from utils. config import *LOG = logfactory.for module()class Common(object):ci= ConfigAttribute("CI')definit (self, defaults-None):self.config = defaults or def run(self):LOG. info(%s CI', self.ci)return getattr( import (.%s.main, % self.ci.lower(), fromlist-[ 'main'l), 'main')(self.config)def main():init log(name-LOG.name, level-'info')iob config ={#ienkins node environmentCI: os.getenv(.CI', .).strip(),yCS: os.getenv('vCS', ..).strip(),JOB TYPE : os.getenv('J0B TYPE'. .).strip(),.CATEGORY": os.getenv(CATEGORY...).strip(),CI SCRIPT DIR': os.getenv('CI SCRIPT DIR', .).strip(),,CI CONFIG DIR': os.getenv('CI CONFIG DIR', .).strip(),WORKSPACE URL': os.getenv( 'WORKSPACE URL', .).strip(./ .),WORKSPACE ROOT': os.getenv( 'WORKSPACE ROOT', .).strip(),#ienkins default environment.J0B URL': os.getenv(.JOB URL').1OB NAME: os.getenv(•JOB NAME'),BUILD URL': os.getenv('BUILD URL'),WORKSPACE': os.getenv( 'WORKSPACE.).JFNKINS URL': os.getenv('JENKINS URL').RUILD NUMBER.: os.getenv('BUILD NUMBER'),BUILD USER ID': os.getenv( 'BUILD USER ID'),-}rc=Common(job_config).run()ifrc l=a:sys.exit(rc)if namemain()==main:
+from utils.helper import *
+from utils.config import *
+
+LOG = logfactory.for_module()
+class Common(object):
+    ci= ConfigAttribute("CI')
+    def __init__(self, defaults=None):
+        self.config = defaults or {}
+
+    def run(self):
+        LOG. info(%s CI', self.ci)
+        return getattr(__import__('%s.main' % self.ci.lower(), fromlist-[ 'main']), 'main')(self.config)
+                  
+def main():
+    init_log(name=LOG.name, level='info')
+    
+    job_config ={
+        #ienkins node environment
+        'CI': os.getenv('CI', '').strip(),
+        'VCS': os.getenv('VCS', '').strip(),
+        'JOB_TYPE': os.getenv('J0B TYPE', '').strip(),
+        'CATEGORY': os.getenv('CATEGORY', '').strip(),
+        'CI_SCRIPT_DIR': os.getenv('CI_SCRIPT_DIR', '').strip(),
+        'CI_CONFIG_DIR': os.getenv('CI_CONFIG_DIR', '').strip(),
+        'WORKSPACE_URL': os.getenv( 'WORKSPACE_URL', '').strip('/ '),
+        'WORKSPACE_ROOT': os.getenv( 'WORKSPACE_ROOT', '').strip(),
+        # jenkins default environment
+        'J0B_URL': os.getenv('JOB_URL'),
+        'JOB_NAME': os.getenv('JOB_NAME'),
+        'BUILD_URL': os.getenv('BUILD_URL'),
+        'WORKSPACE': os.getenv('WORKSPACE'),
+        'JENKINS_URL': os.getenv('JENKINS URL').
+        'BUILD_NUMBER.: os.getenv('BUILD_NUMBER'),
+        'BUILD_USER_ID': os.getenv('BUILD_USER_ID')
+    }
+
+    rc = Common(job_config).run()
+    if rc != 0:
+        sys.exit(rc)
+        
+if __name__ == '__main__':
+    main()
