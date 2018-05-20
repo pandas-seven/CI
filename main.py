db16891f76adb99b93c825721061489abc92434f
pandas-seven <34061140+pandas-seven@users.noreply.github.com>
2018-05-20 06:27:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,22 @@ from utils.helper import *
 from utils.config import *
 
 LOG = logfactory.for_module()
+
+
 class Common(object):
     ci= ConfigAttribute('CI')
+
     def __init__(self, defaults=None):
         self.config = defaults or {}
 
     def run(self):
-        LOG. info('%s CI' % self.ci)
+        LOG.info('%s CI' % self.ci)
         return getattr(__import__('%s.main' % self.ci.lower(), fromlist=['main']), 'main')(self.config)
-                  
+
+
 def main():
     init_log(name=LOG.name, level='info')
-    
+
     job_config ={
         # jenkins node environment
         'CI': os.getenv('CI', '').strip(),
@@ -32,7 +36,7 @@ def main():
         'BUILD_URL': os.getenv('BUILD_URL'),
         'WORKSPACE': os.getenv('WORKSPACE'),
         'JENKINS_URL': os.getenv('JENKINS_URL'),
-        'BUILD_NUMBER: os.getenv('BUILD_NUMBER'),
+        'BUILD_NUMBER': os.getenv('BUILD_NUMBER'),
         'BUILD_USER_ID': os.getenv('BUILD_USER_ID'),
     }
 
